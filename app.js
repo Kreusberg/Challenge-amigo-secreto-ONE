b04aeeadfda49911a3e8997bd7958520fc3548d7
Lucas Lodi Kreusberg <gitkreusberg@gmail.com>
2025-01-23 21:37:16 -0300

    implement adicionarAmigo method

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomes = [];
+
+function adicionarAmigo() {
+    let resposta = document.querySelector("input");
+
+    if (resposta.value == "" || resposta.value.length == 0) {
+        alert("Por favor. Digite um valor válido");
+    } else {
+        nomes.push(resposta.value);
+        resposta.value = "";
+    }
+
+}
