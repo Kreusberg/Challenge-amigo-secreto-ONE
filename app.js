4ceb8333014d29b38cea1e206ce1f2b9f9676b57
Lucas Lodi Kreusberg <gitkreusberg@gmail.com>
2025-01-27 20:31:40 -0300

    implement atualizarListaAmigos function

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let nomes = [];
+let amigos = [];
+let listaAmigos = document.querySelector("ul");
 
 function adicionarAmigo() {
     let resposta = document.querySelector("input");
 
+    console.log(resposta.value);
+
     if (resposta.value == "" || resposta.value.length == 0) {
         alert("Por favor. Digite um valor válido");
     } else {
-        nomes.push(resposta.value);
-        resposta.value = "";
+        amigos.push(resposta.value);
+        console.log(amigos);
+
+        // chama a função responsável por adicionar o nome do amigo na lista "listaAmigos"
+        atualizarListaAmigos();
+
     }
+
+function atualizarListaAmigos() {
+
+    listaAmigos.innerHTML = "";
+
+    // element aqui representa o nome do amigo digitado no campo "resposta" (tag input)
+    amigos.forEach(element => {
+        let li = document.createElement("li");
+        li.innerHTML = element;
+        listaAmigos.appendChild(li);
+    })
+
+}
 
 }
